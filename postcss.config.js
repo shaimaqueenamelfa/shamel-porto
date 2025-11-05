2ec08387ea1c386ef91bad686f3ea5f99a27be88
shaima_queena_melfa <shaima.queena19@smk.belajar.id>
2025-11-05 15:20:24 +0700

    fix: correct PostCSS config for Tailwind v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
